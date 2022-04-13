8194b00bca77c0b2a939d12f1a4f2c36d26e26c5
Romain Beauxis <toots@rastageeks.org>
2022-04-13 13:35:25 -0500

    Update endianess.c

--- src/config/endianess.c
+++ src/config/endianess.c
@@ -3,17 +3,17 @@
 
 enum
 {
-    OCAML_LAME_LITTLE_ENDIAN = 0x0100,
-    OCAML_LAME_BIG_ENDIAN = 0x0001,
+    OCAML_SHINE_LITTLE_ENDIAN = 0x0100,
+    OCAML_SHINE_BIG_ENDIAN = 0x0001,
 };
 
 static const union { unsigned char bytes[2]; uint16_t value; } host_order =
     { { 0, 1 } };
 
-CAMLprim value ocaml_lame_is_big_endian(value unit) {
+CAMLprim value ocaml_shine_is_big_endian(value unit) {
   CAMLparam0();
 
-  if (host_order.value == OCAML_LAME_BIG_ENDIAN)
+  if (host_order.value == OCAML_SHINE_BIG_ENDIAN)
     CAMLreturn(Val_bool(1));
 
   CAMLreturn(Val_bool(0));
